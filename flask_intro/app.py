cad0a09165d095114081d3de7cc84c62bbc18007
Jacob Koehler <jacobkoehler@Jacobs-iMac.local>
2020-05-20 22:06:02 -0400

    ann

--- flask_intro/app.py
+++ flask_intro/app.py
@@ -7,6 +7,14 @@ import numpy as np
 df = pd.DataFrame({'x': np.random.random(10),
 'y': np.random.random(10)}).to_html()
 
+#lookup the top 10 county's by state through cases in USA
+def state_lookup(state):
+  df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+  state_df = df.loc[df['state'] == state]
+  state_df = state_df.groupby('county')[['cases', 'deaths']].sum()
+  return state_df.nlargest(10, 'cases').plot(kind = 'bar')
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,10 +29,10 @@ def name():
 @app.route('/form', methods = ["GET", "POST"])
 def formpage():
     if request.method == "POST":
-        name = request.form.get('person')
-        age = request.form.get('age')
-        d = {'name': name, 'age': age}
-        return render_template('submitted.html', name = d)
+        state = request.form.get('state')
+        
+        d = state_lookup(state)
+        return render_template('submitted.html', state_table = d)
     return render_template('form.html')
 
 if __name__ == '__main__':
